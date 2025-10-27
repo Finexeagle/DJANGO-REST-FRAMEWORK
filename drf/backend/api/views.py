1b5e631d0048aa4dbff05fc1c917d3a0e8b31097
Sanuka Kumarasinghe <155355976+Finexeagle@users.noreply.github.com>
2025-10-27 14:51:26 -0400

    created the Data model, send model data thorugh api and used model_to_dict

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,23 +1,15 @@
 from django.shortcuts import render
+from django.forms.models import model_to_dict
 from django.http import JsonResponse
 import json
-# Create your views here.
-# def api(request, *args, **kwargs):
-#     print(request.body)
-#     return(JsonResponse({"data":2}))
+from api.models import Product
 
 def api(request, *args, **kwargs):
-    body = request.body # byte string of json data
+    model = Product.objects.all().order_by("?").first()
+    print(type(model))
     data = {}
-    try:
-        data = json.loads(body) # conveerts byte string to a json string
-    except:
-        pass
-    print(data)
-    print(request.GET) # getting the Query params 
-    print(request.headers) # django object
-    print(request.content_type) # django object
-    print(json.dumps(dict(request.headers))) # django object
-    return(JsonResponse({"data":2}))
+    if model:
+        data = model_to_dict(model, fields=['id', 'title'])
+    return JsonResponse(data)
 
 
--- drf/pyclient/base.py
+++ drf/pyclient/base.py
@@ -1,5 +1,5 @@
 import requests
 
-endpoint = "http://localhost:8000/api/?abc=123"  # abc=123 is also a query parameter
+endpoint = "http://localhost:8000/api/"  # abc=123 is also a query parameter
 response = requests.get(endpoint, params={"ltr":123}, json={"message":"hi"}) # params means query parameters
 print(response.text)
