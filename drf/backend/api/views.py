016095ca058320ca5f825eb2814645bd3ac7bff3
Sanuka Kumarasinghe <155355976+Finexeagle@users.noreply.github.com>
2025-10-29 10:30:43 -0400

    Converted the normal view Funcs to rest API views, Created Model Forms and Serializers

--- drf/backend/api/views.py
+++ drf/backend/api/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
 from api.models import Product
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import ProductSerializers
+
+@api_view(["GET"])
 def api(request, *args, **kwargs):
-    model = Product.objects.all().order_by("?").first()
-    print(type(model))
+    instance = Product.objects.all().order_by("?").first()
+    print(type(instance))
     data = {}
-    if model:
-        data = model_to_dict(model, fields=['id', 'title'])
-    return JsonResponse(data)
+    if instance:
+        data = ProductSerializers(instance).data
+    return Response(data)
 
 
--- drf/pyclient/base.py
+++ drf/pyclient/base.py
@@ -1,5 +1,6 @@
 import requests
 
 endpoint = "http://localhost:8000/api/"  # abc=123 is also a query parameter
-response = requests.get(endpoint, params={"ltr":123}, json={"message":"hi"}) # params means query parameters
+response = requests.get(endpoint, params={"ltr":123}) # params means query parameters
+
 print(response.text)
